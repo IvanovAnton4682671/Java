3d53d0379c2ce8d50ae283dd825a9d4c56b12389
IvanovAnton4682671 <anton-ivanov-080203@mail.ru>
2023-05-03 00:37:56 +0300

    Доработка визуала, тест с poi

--- ProbabilityTheoryProject/src/Main.java
+++ ProbabilityTheoryProject/src/Main.java
@@ -18,5 +18,27 @@ public class Main
             ex.printStackTrace();
         }
         new Form();
+        //
+        //тест
+        //
+        String[] specialSymbols = {"xᵢ", "xᵢ₊₁", "xᵢ - xᵢ₊₁", "nᵢ", "n₃", "X̅", "ʙ", "x̅ᵧ", "ρyx", "y̅", "x̅", "y̅ₓ"};
+        PoiMainClass poiMainClass = new PoiMainClass("test1", "Times New Roman", 14);
+        poiMainClass.newParagraph();
+        poiMainClass.addText("test2");
+        poiMainClass.addText("bimba");
+        poiMainClass.newParagraph();
+        poiMainClass.addTextBolt("aboba ");
+        poiMainClass.addText("tank");
+        //poiMainClass.newParagraph();
+        poiMainClass.initTable(2, 2);
+        poiMainClass.addTaleItem("leopard", 0, 0);
+        poiMainClass.addTaleItem("ferdinand", 0, 1);
+        poiMainClass.addTaleItem("brymbar", 1, 0);
+        poiMainClass.addTaleItem("is-7", 1, 1);
+        poiMainClass.newParagraph();
+        poiMainClass.addPicture("src/dopRes/iconImage.png", 980/2, 980/2);
+        poiMainClass.newParagraph();
+        poiMainClass.addText(specialSymbols[3] + "kek" + 5);
+        poiMainClass.printToFile();
     }
 }
